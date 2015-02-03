10d1ff7418e73aeab88971d78bfeec2e81a669d3
Bharat_sahu <bharat91.sahu@gmail.com>
2015-02-03 13:08:05 -0800

    again comment at 5:30

---
+++ pizzashop-0.1.0.BUILD-SNAPSHOT/src/main/java/com/myproject/pizzashop/domain/PizzaController.java
@@ -0,0 +1,10 @@
+package com.myproject.pizzashop.domain;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/pizzas")
+@Controller
+@RooWebScaffold(path = "pizzas", formBackingObject = Pizza.class)
+public class PizzaController {
+}
---
+++ pizzashop-0.1.0.BUILD-SNAPSHOT/src/test/java/com/myproject/pizzashop/domain/BaseDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.myproject.pizzashop.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Base.class)
+public class BaseDataOnDemand {
+}
